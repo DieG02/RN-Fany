05af8dcf64d1e92be2e6d69327c15c3346a2b7e7
DieG02 <43993554@alejandrovolta.edu.ar>
2021-08-06 11:57:23 -0300

    set header from Song component

--- src/Views/Song/index.js
+++ src/Views/Song/index.js
@@ -3,7 +3,9 @@ import {
   View,
   Text,
   StyleSheet,
-  StatusBar
+  StatusBar,
+  SafeAreaView,
+  TouchableOpacity
 } from 'react-native'
 import TrackPlayer from 'react-native-track-player'
 
@@ -38,7 +40,7 @@ const start = async () => {
   };
   await TrackPlayer.add([track3]);
   // Start playing it
-  await TrackPlayer.play();
+  // await TrackPlayer.play();
 };
 
 
@@ -47,19 +49,46 @@ function Song() {
   useEffect(() => {
     start();
   }, [])
+
   return (
-    <View style={styles.container}>
-      <StatusBar barStyle={'light-content'} />
-      <Text>
-        Song Screen
-      </Text>
+    <View style={{ flex: 1 }}>
+      <StatusBar barStyle='dark-content' translucent={false} backgroundColor={"#f00"} />
+      <SafeAreaView style={styles.container}>
+        <View style={styles.header}>
+          <TouchableOpacity
+            style={styles.icon}
+            onPress={() => console.log('Back')}
+          >
+            <Text>{`<`}</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.icon}
+            onPress={() => console.log('Menu')}
+          >
+            <Text>{`=`}</Text>
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    alignItems: 'center',
+    width: '100%',
+    height: '100%',
+  },
+  header: {
+    height: 50,
+    width: '100%',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flexDirection: 'row',
+    backgroundColor: '#0f0',
+  },
+  icon: {
+    width: '11%',
     justifyContent: 'center',
     alignItems: 'center',
   }
